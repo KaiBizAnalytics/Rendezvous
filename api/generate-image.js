ebb075a79aa6b6b1314d227438ae62033747a78d
Kai Li <kai.li0702@hotmail.com>
2026-03-25 11:41:49 -0700

    changed gen image size back to 1536x1024

--- api/generate-image.js
+++ api/generate-image.js
@@ -139,7 +139,7 @@ module.exports = async function handler(req, res) {
     const response = await openai.images.generate({
       model:   'gpt-image-1',
       prompt,
-      size:    '1920x1080',
+      size:    '1536x1024',
       quality: 'medium',
       n:       1,
     });
